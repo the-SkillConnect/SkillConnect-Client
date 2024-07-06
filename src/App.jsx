8d3c982a9b12a54fb40bce7b2fc09bd7e90f0a1a
Milad Rasouli <milad75rasouli@gmail.com>
2024-07-06 19:56:32 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import HomePage from './components/home/HomePage'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <HomePage></HomePage>
+    </>
+  )
+}
+
+export default App
